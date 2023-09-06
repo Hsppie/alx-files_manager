1848639d64a2a51520463794cc7ffddfe93f29a7
Hsppie <wambimicheal@gmail.com>
2023-09-06 20:51:27 +0300

    updates

--- utils/redis.js
+++ utils/redis.js
@@ -1,24 +1,33 @@
-import {createClient} from 'redis'
-import { promisify } from 'util'
+import { promisify } from 'util';
+import { createClient } from 'redis';
 
-class RedisClient{
-    constructor(){
-        this.client = createClient();
-        this.isClientConnected = true;
-        this.client.on('error', (err)=>{
-            console.error('Redis client failed to connect:', err.message);
-        });
-        this.client.on('connect', ()=>{
-            this.isClientConnected =true;
-        });
-    }
-     /**
+/**
+ * Represents a Redis client.
+ */
+class RedisClient {
+  /**
+   * Creates a new RedisClient instance.
+   */
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  /**
    * Checks if this client's connection to the Redis server is active.
    * @returns {boolean}
    */
   isAlive() {
     return this.isClientConnected;
   }
+
   /**
    * Retrieves the value of a given key.
    * @param {String} key The key of the item to retrieve.
@@ -49,5 +58,6 @@ class RedisClient{
     await promisify(this.client.DEL).bind(this.client)(key);
   }
 }
+
 export const redisClient = new RedisClient();
 export default redisClient;
